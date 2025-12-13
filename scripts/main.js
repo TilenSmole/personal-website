ed1dab041363ea6e23c5fae4a8a7553a3a86bdd2
TilenSmole <idiomassonmislove@gmail.com>
2025-12-13 11:51:53 +0100

    about me button fix

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,7 @@ function showMyInfo() {
     var home = document.getElementById("home");
     var showMyInfo = document.getElementById("showMyInfo");
     var aboutMe = document.getElementById("aboutMe");
-    var aboutMe = document.getElementById("aboutMe");
-    var homeNav = document.getElementsByClassName("homeNav");
+    var showHome = document.getElementById("showHome");
 
     showHome = home.style.display == "none"
 
@@ -12,19 +11,17 @@ function showMyInfo() {
         home.style.display = "flex";
         showMyInfo.innerText = "O meni";
         aboutMe.style.display = "none";
-        homeNav.href = "#home"
     }
     else {
         home.style.display = "none";
         showMyInfo.innerText = "Nazaj";
         aboutMe.style.display = "flex";
-                homeNav.href = "#aboutMe"
 
     }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-   const loginButton = document.getElementById("showMyInfo").addEventListener('click', showMyInfo);
+   document.getElementById("showMyInfo").addEventListener('click', showMyInfo);
 
 });
